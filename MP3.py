28b9ee695392f1714dbfb6c3991d713539dad6ad
BuyutGans <52874600+BuyutGans@users.noreply.github.com>
2019-11-12 20:04:58 +0700

    Update MP3.py

--- MP3.py
+++ MP3.py
@@ -1,10 +1,20 @@
 import os
-biru = "33[0;36m"
-hijau = ""
-print (20*biru + "=")
-print('+Tools Download Mp3+')
-print('+Sederhana         +')
-print('+Autho:Buyut       +')
-print(20*biru + '=')
-l = input('Link Youtubenya: ')
-os.system("youtube-dl --extract-audio --audio-format mp3 " + l)
+def jalan():
+  biru = "33[0;36m"
+  hijau = ""
+  print (20*biru + "=")
+  print('+Tools Download Mp3+')
+  print('+Sederhana         +')
+  print('+Autho:Buyut       +')
+  print(20*biru + '=')
+  l = input('Link Youtubenya: ')
+  os.system("youtube-dl --extract-audio --audio-format mp3 " + l)
+
+print("Install Bahan? Y/T")
+c = input("")
+if c == Y | c == y:
+  os.system("pkg install python2")
+  os.system("pip2 install youtube-dl")
+  jalan()
+else:
+  jalan()
